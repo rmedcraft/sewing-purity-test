7825ea3bbd5b4da4d35029c2dca95931158d437e
rmedcraft <rowanmedcraft04@gmail.com>
2026-01-13 11:48:24 -0500

    checkboxes count the user total

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -1,10 +1,16 @@
 export default function Checkbox(props: any) {
-    const { question, index } = props
+    const { question, index, setTotal } = props
 
     return (
         <div className="flex flex-row gap-2 align-top justify-start">
-            <p className="select-none">{index + 1}.</p>
-            <input id={`check${index}`} type="checkbox" className="mt-0" />
+            <p className="select-none">{`${index + 1}.`.padEnd(4, " ")}</p>
+            <input id={`check${index}`} type="checkbox" onChange={(e: any) => {
+                if (e.target.checked) {
+                    setTotal((total: number) => total + 1)
+                } else {
+                    setTotal((total: number) => total - 1)
+                }
+            }} />
             <label htmlFor={`check${index}`} className="select-none">{question}</label>
         </div>
     )
